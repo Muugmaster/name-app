24dd5dc13429daf5c31e7db4d7148d4019b482b1
Muukkonen <niko@muukkonen.info>
2020-12-28 14:09:45 +0200

    Added footer.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import Footer from './components/footer/Footer';
 import SearchBar from './components/searchBar/SearchBar';
 import Totalnames from './components/totalnames/Totalnames';
 
@@ -5,11 +6,14 @@ import './styles.css';
 
 function App() {
   return (
+    <>
     <div className="container">
       <h1>Solita Names App</h1>
       <Totalnames />
       <SearchBar />
     </div>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import './styles.css';
+
+const Footer = () => {
+    return (
+        <footer>
+            Name Application for Solita Dev Academy 2021.
+        </footer>
+    )
+}
+
+export default Footer
